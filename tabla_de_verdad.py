1bf2e6c36aa00a9f758fc57db703455df334b3d8
Anthony López Tinoco <63381943+4nth0nySLT@users.noreply.github.com>
2021-06-05 16:11:56 -0500

    Update tabla_de_verdad.py

--- tabla_de_verdad.py
+++ tabla_de_verdad.py
@@ -75,3 +75,22 @@ def tabla_bruta (numero):
 		memoria=memoria/2
 	print(time.time()-ini)
 	return matriz
+
+def tabla_version3 (numero):
+	ini=time.time()
+	total=2**numero
+	matriz2=[]
+	matriz=tabla_bruta(numero)
+	for index in range(0,int(total/2)):
+		matriz2.append([])
+		matriz2[index].append(matriz[index])
+		base=index
+		for veces in range(numero-1):
+			base=base+total
+			matriz2[index].append(matriz[base])
+		#print(matriz2[index],(index/total)*100,"%",end='             \r')
+	print("")
+	print("--------------------------100%--------------------------")
+	matriz2+=np.logical_not(matriz2).tolist()
+	print(time.time()-ini)
+	return matriz2
