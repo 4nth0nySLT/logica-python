92bba6c6373270ecd4ef0c5facd872cbbc5ce65e
Anthony López Tinoco <63381943+4nth0nySLT@users.noreply.github.com>
2021-06-05 20:47:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tabla_de_verdad
-x=tabla_de_verdad.tabla(5)
-for i in range(len(x)):
-	print(x[i])
+x=tabla_de_verdad.tabla(20)
+x=tabla_de_verdad.tabla_version2(20)
+x=tabla_de_verdad.tabla_version3(20)
+x=tabla_de_verdad.tabla_version4(20)
+
