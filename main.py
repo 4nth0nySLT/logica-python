52641f3e44f95d6a718630583a0aa179aa190a84
Anthony López Tinoco <63381943+4nth0nySLT@users.noreply.github.com>
2021-06-05 20:48:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tabla_de_verdad
+print("x=tabla_de_verdad.tabla(20)")
 x=tabla_de_verdad.tabla(20)
+print("x=tabla_de_verdad.tabla_version2(20)")
 x=tabla_de_verdad.tabla_version2(20)
+print("x=tabla_de_verdad.tabla_version3(20)")
 x=tabla_de_verdad.tabla_version3(20)
+print("x=tabla_de_verdad.tabla_version4(20)")
 x=tabla_de_verdad.tabla_version4(20)
 
